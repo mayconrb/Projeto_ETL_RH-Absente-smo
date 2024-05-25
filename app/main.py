80718e0c679044d3d528f9648b39a6147124d92a
mayconrb <mayconrb00@gmail.com>
2024-05-25 12:55:38 -0300

    Atualização do projeto. Processando o input dos arquivo, concatenando arquivos e realizando o output com um único arquivo com todos os arquivos

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from pipe.extract import extratc_excel
+from pipe.load import load_excel
+from pipe.transform import transf_data
+
+
+if __name__ == "__main__":
+    extract_excel = extratc_excel('data/input')
+    transf_data = transf_data(extract_excel)
+    load_excel = load_excel(transf_data, 'arquivo', 'data/output')
+
